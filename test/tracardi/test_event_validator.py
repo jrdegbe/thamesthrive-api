828f5d38f678f52010be3a8c8bce0ec9cfc618bf
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2022-01-14 09:54:59 +0100

    validation fix (#41)

---
+++ test/tracardi/test_event_validator.py
@@ -0,0 +1,58 @@
+import jsonschema
+
+from tracardi.domain.event_payload_validator import EventPayloadValidator
+from tracardi.service.event_validator import validate
+from tracardi.service.notation.dot_accessor import DotAccessor
+from tracardi.exceptions.exception import EventValidationException
+
+def test_should_read_the_whole_object():
+    dot = DotAccessor(payload={"test": 1})
+    validator = EventPayloadValidator(
+        validation={"payload@...": {"type": "object"}},
+        event_type="page-view",
+        name="test",
+        enabled=True
+    )
+
+    # try:
+    validate(dot, validator)
+    # except Exception:
+    #     assert False
+
+
+def test_should_read_the_part_of_object():
+    dot = DotAccessor(payload={"test": {"a": 1}})
+    validator = EventPayloadValidator(
+        validation={"payload@test": {"type": "object"}},
+        event_type="page-view",
+        name="test",
+        enabled=True
+    )
+
+    try:
+        validate(dot, validator)
+    except Exception:
+        assert False
+
+
+def test_should_differentiate_types():
+    dot = DotAccessor(payload={"list": ["a", "b", "c"]})
+    validator = EventPayloadValidator(
+        validation={"payload@list": {"type": "array"}},
+        event_type="page-view",
+        name="test",
+        enabled=True
+    )
+
+    try:
+        validate(dot, validator)
+    except Exception:
+        assert False
+
+    dot = DotAccessor(payload={"list": "not_a_list_for_sure"})
+
+    try:
+        validate(dot, validator)
+    except EventValidationException:
+        assert True
+
